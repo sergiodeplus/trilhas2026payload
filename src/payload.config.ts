42d1c2730d36aa594371e7204420ecda4a9ef56a
Sérgio Araújo <zonaeducacional@gmail.com>
2026-03-03 22:38:56 -0300

    Fix: Add SSL rejectUnauthorized=false to Postgres connection pooler

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -31,6 +31,12 @@ export default buildConfig({
   db: postgresAdapter({
     pool: {
       connectionString: process.env.DATABASE_URL || '',
+      max: 10,
+      idleTimeoutMillis: 30000,
+      connectionTimeoutMillis: 2000,
+      ssl: {
+        rejectUnauthorized: false
+      }
     },
   }),
   sharp,
